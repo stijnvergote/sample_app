f8ec31d572493c2f90bf4c695d3877cad69deff6
Stijn Vergote <vergootje3@hotmail.com>
2014-02-14 11:27:13 +0100

    Finish user singup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -30,11 +30,11 @@ describe "UserPages" do
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
-      end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+    end
     end
   end
 
